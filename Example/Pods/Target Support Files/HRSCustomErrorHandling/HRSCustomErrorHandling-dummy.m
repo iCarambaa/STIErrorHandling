e54f0cfd4b568f7eca0b6e32e970ca16116b4aec
Sven Titgemeyer <s.titgemeyer@titgemeyer-it.de>
2018-09-08 17:42:27 +0200

    Add Pods to repo.

---
+++ Example/Pods/Target Support Files/HRSCustomErrorHandling/HRSCustomErrorHandling-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HRSCustomErrorHandling : NSObject
+@end
+@implementation PodsDummy_HRSCustomErrorHandling
+@end
---
+++ Example/Pods/Target Support Files/Pods-HRSCustomErrorHandling/Pods-HRSCustomErrorHandling-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HRSCustomErrorHandling : NSObject
+@end
+@implementation PodsDummy_Pods_HRSCustomErrorHandling
+@end
